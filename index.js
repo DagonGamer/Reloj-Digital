5bb3fb88d6eee579fcfaeacda26e39030b227835
Dagongamer <davidgonzalezleon21@gmail.com>
2025-09-02 23:32:33 +0200

    Sincronizar configuracion

---
+++ index.js
@@ -0,0 +1,18 @@
+var Cargar = async () => {
+    await tokenClient.requestAccessToken();
+
+    // Busca la configuración en el drive
+    let ArchivosDataFolder;
+    try {
+        ArchivosDataFolder = await gapi.client.drive.files.list({
+            pageSize: 10,
+            spaces: "AppDataFolder"
+        });
+    } catch (err) {
+        document.getElementById('content').innerText = err.message;
+        return;
+    }
+
+    console.log(ArchivosDataFolder.data.files);
+
+}
