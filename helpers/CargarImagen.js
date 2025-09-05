41994fe4c22a7648b1d9252be481e265c724e276
Dagongamer <davidgonzalezleon21@gmail.com>
2025-09-05 12:10:51 +0200

    Orden de imágenes

--- helpers/CargarImagen.js
+++ helpers/CargarImagen.js
@@ -5,9 +5,25 @@ var CargarImagen = async (selector) => {
         await IDaIMG(OrdenFotos[IdxImagen][0])
             .then(file => OrdenFotos[IdxImagen][0] = URL.createObjectURL(file));
     element.querySelector("img.Fondo").src = OrdenFotos[IdxImagen][0];
-    
+
     IdxImagen++;
     if (IdxImagen == OrdenFotos.length)
         IdxImagen = 0;
+
+}
+
+var CambiarImagen = async () => {
+
+    let NoWaiting = document.querySelector("div.Reloj.NoWaiting");
+    let Waiting = document.querySelector("div.Reloj.Waiting");
+
+    NoWaiting.style.opacity = 0;
+    NoWaiting.classList.remove("NoWaiting");
+    Waiting.classList.add("NoWaiting");
+    Waiting.classList.remove("Waiting");
+    NoWaiting.classList.add("Waiting");
+    NoWaiting.style.opacity = 1;
+
+    CargarImagen("div.Reloj.Waiting");
 
 }
