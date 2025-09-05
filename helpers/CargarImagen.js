a37f3910254857da48f0193413733645da2bd2c7
Dagongamer <davidgonzalezleon21@gmail.com>
2025-09-05 10:50:10 +0200

    Orden de imágenes

--- helpers/CargarImagen.js
+++ helpers/CargarImagen.js
@@ -1,10 +1,13 @@
 var CargarImagen = async (selector) => {
 
     let element = document.querySelector(selector);
-    if (OrdenFotos[IdxImagen][0].contains("blob:https")) 
+    if (!OrdenFotos[IdxImagen][0].includes("blob:https")) 
         await IDaIMG(OrdenFotos[IdxImagen][0])
             .then(file => OrdenFotos[IdxImagen][0] = URL.createObjectURL(file));
     element.querySelector("img.Fondo").src = OrdenFotos[IdxImagen][0];
+    
     IdxImagen++;
+    if (IdxImagen == OrdenFotos.length)
+        IdxImagen = 0;
 
 }
