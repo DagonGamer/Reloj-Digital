dafdca5f524f8353fd8d249c5a7be5af43a4e221
Dagongamer <davidgonzalezleon21@gmail.com>
2025-09-08 17:18:10 +0200

    Creación de Menú

---
+++ helpers/Añadir carpetas o imágenes.js
@@ -0,0 +1,15 @@
+let importarImagenOCarpeta = async datos => {
+    console.log(datos);
+}
+
+var anadirImagenesOCarpetas = async () => {
+    pickr = new google.picker.PickerBuilder()
+            .addView(google.picker.ViewId.DOCS_IMAGES)
+            .addView(google.picker.ViewId.FOLDERS)
+            .setOAuthToken(accessToken)
+            .setDeveloperKey('AIzaSyCO7_To8On1vcHoEcjWbMDnOlFPkj-KRbo')
+            .setCallback(importarImagenOCarpeta)
+            .setAppId('808752279453-v65b4epu2pjqj8ee5qnj2fe9l7odgqpg.apps.googleusercontent.com')
+            .build();
+        pickr.setVisible(true);
+}
