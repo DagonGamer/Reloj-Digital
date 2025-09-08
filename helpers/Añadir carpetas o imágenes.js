f9f9c18d7620191bec8dc67ac491e45e01aa5caa
Dagongamer <davidgonzalezleon21@gmail.com>
2025-09-08 17:42:15 +0200

    Creación de Menú

--- helpers/Añadir carpetas o imágenes.js
+++ helpers/Añadir carpetas o imágenes.js
@@ -3,15 +3,25 @@ let importarImagenOCarpeta = async datos => {
 }
 
 var anadirImagenesOCarpetas = async () => {
+
+    let vista = new google.picker.DocsView(google.picker.ViewId.DOCS)
+        // 1. Fija la carpeta raíz al abrir
+        .setParent('root')
+        // 2. Permite navegar y seleccionar carpetas
+        .setIncludeFolders(true)
+        .setSelectFolderEnabled(true)
+        // 3. Solo muestra MIME types de imagen
+        .setMimeTypes('image/png,image/jpeg,image/jpg,image/gif,image/webp')
+        .setQuery(Config.Imagenes.map(el => `not id = '${el.ID}'`).join(' and '));
+
     pickr = new google.picker.PickerBuilder()
-        .addView(google.picker.ViewId.DOCS_IMAGES)
-        .addView(google.picker.ViewId.FOLDERS)
-        .addView(new google.picker
-            .setQuery(Config.Imagenes.map(el => `not id = '${el.ID}'`).join(' and ')))
+        .addView(vista)
         .setOAuthToken(accessToken)
         .setDeveloperKey('AIzaSyCO7_To8On1vcHoEcjWbMDnOlFPkj-KRbo')
         .setCallback(importarImagenOCarpeta)
+        .enableFeature(google.picker.Feature.MULTISELECT)
         .setAppId('808752279453-v65b4epu2pjqj8ee5qnj2fe9l7odgqpg.apps.googleusercontent.com')
         .build();
+        
     pickr.setVisible(true);
 }
