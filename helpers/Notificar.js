f3c1723dcc2bf87e17c7332bd1d8ff57d23f8ab6
Dagongamer <davidgonzalezleon21@gmail.com>
2025-09-09 17:49:48 +0200

    Pequeños ajustes

--- helpers/Notificar.js
+++ helpers/Notificar.js
@@ -3,9 +3,19 @@ var Notificar = (info) => {
     let div = document.createElement("div");
     div.innerHTML = `
         <button>x</button>
-        <p>${info}</p>
+        <p>hfdsklkfds</p>
     `;
     document.querySelector("div.Notification").insertBefore(div, document.querySelector("div.Notification div"));
+	div.style.position = "absolute";
+	div.style.margin = "-" + div.getBoundingClientRect().height + "px 0px";
+	div.style.top = "-50dvh";
+	div.style.transition = "all 0.5s";
+
+	setTimeout(() => {
+		div.style.margin = "0.5rem 0px";
+		div.style.position = "relative";
+		div.style.top = "0dvh";
+    }, 100);
 
     setTimeout(() => {
         div.style.opacity = 0;
