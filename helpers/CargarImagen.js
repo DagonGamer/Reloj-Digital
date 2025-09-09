9bfe73f6d1383023a26d1e2dc4664c4ef0bcf9f6
Dagongamer <davidgonzalezleon21@gmail.com>
2025-09-09 16:22:56 +0200

    Imagenes con misma relacion de aspecto que la pantalla son las únicas que se muestran

--- helpers/CargarImagen.js
+++ helpers/CargarImagen.js
@@ -6,10 +6,10 @@ var DescargarImagenes = async () => {
         if (!OrdenFotos[i][0].includes("blob:https"))
             await IDaIMG(OrdenFotos[i][0])
                 .then(file => OrdenFotos[i][0] = URL.createObjectURL(file));
-        document.querySelector("body").style.setProperty("--Progress", (i+1) / OrdenFotos.length * 100);
-        document.querySelector("div.RedDeClick p.Porcentaje").innerText = `${Math.floor((i+1) / OrdenFotos.length * 1000) / 10}%`;
-        if (Percentage != Math.floor((i+1) / OrdenFotos.length * 10) * 10) {
-            Percentage = Math.floor((i+1) / OrdenFotos.length * 10) * 10;
+        document.querySelector("body").style.setProperty("--Progress", (i + 1) / OrdenFotos.length * 100);
+        document.querySelector("div.RedDeClick p.Porcentaje").innerText = `${Math.floor((i + 1) / OrdenFotos.length * 1000) / 10}%`;
+        if (Percentage != Math.floor((i + 1) / OrdenFotos.length * 10) * 10) {
+            Percentage = Math.floor((i + 1) / OrdenFotos.length * 10) * 10;
             console.log(`${Percentage}% de las imágenes cargadas.`);
         }
     }
@@ -18,11 +18,12 @@ var DescargarImagenes = async () => {
 
 }
 
-var CargarImagen = async (selector) => {
+var CargarImagen = async (selector, intento = 0) => {
 
     if (OrdenFotos.length == 0) return;
 
     let element = document.querySelector(selector);
+    let imgFondo = element.querySelector("img.Fondo");
 
     // Busca el estilo de la imagen
     let Estilo = Config.Estilos.find(el => el.Nombre == OrdenFotos[IdxImagen][1]);
@@ -31,60 +32,73 @@ var CargarImagen = async (selector) => {
     if (!OrdenFotos[IdxImagen][0].includes("blob:https"))
         await IDaIMG(OrdenFotos[IdxImagen][0])
             .then(file => OrdenFotos[IdxImagen][0] = URL.createObjectURL(file));
-    element.querySelector("img.Fondo").src = OrdenFotos[IdxImagen][0];
+    imgFondo.src = OrdenFotos[IdxImagen][0];
 
-    // Carga los textos
-    for (let i = 0; i < Estilo.Textos.length; i++) {
-        let texto = Estilo.Textos[i];
+    imgFondo.onload = async () => {
 
-        let nuevoTexto = document.createElement("p");
-        nuevoTexto.setAttribute("Input", texto.Contenido);
-        nuevoTexto.style.height = texto.Altura;
-        nuevoTexto.style.width = texto.Ancho;
-
-        switch (texto.AnclajeX) {
-            case "Derecha":
-                nuevoTexto.style.right = texto.DesplazamientoX;
-                break;
-            case "Izquierda":
-                nuevoTexto.style.left = texto.DesplazamientoX;
-                break;
+        if ((imgFondo.naturalHeight > imgFondo.naturalWidth) != (imgFondo.height > imgFondo.width)) {
+            IdxImagen++;
+            if (IdxImagen == OrdenFotos.length)
+                IdxImagen = 0;
+            if (intento <= OrdenFotos.length+2)
+                await CargarImagen(selector, intento+1);
+            else return;
         }
 
-        switch (texto.AnclajeY) {
-            case "Arriba":
-                nuevoTexto.style.top = texto.DesplazamientoY;
-                break;
-            case "Abajo":
-                nuevoTexto.style.bottom = texto.DesplazamientoY;
-                break;
+        // Carga los textos
+        for (let i = 0; i < Estilo.Textos.length; i++) {
+            let texto = Estilo.Textos[i];
+
+            let nuevoTexto = document.createElement("p");
+            nuevoTexto.setAttribute("Input", texto.Contenido);
+            nuevoTexto.style.height = texto.Altura;
+            nuevoTexto.style.width = texto.Ancho;
+
+            switch (texto.AnclajeX) {
+                case "Derecha":
+                    nuevoTexto.style.right = texto.DesplazamientoX;
+                    break;
+                case "Izquierda":
+                    nuevoTexto.style.left = texto.DesplazamientoX;
+                    break;
+            }
+
+            switch (texto.AnclajeY) {
+                case "Arriba":
+                    nuevoTexto.style.top = texto.DesplazamientoY;
+                    break;
+                case "Abajo":
+                    nuevoTexto.style.bottom = texto.DesplazamientoY;
+                    break;
+            }
+
+            nuevoTexto.style.fontSize = texto.Tamano;
+            nuevoTexto.style.color = texto.Color;
+            nuevoTexto.style.opacity = texto.Opacidad;
+            nuevoTexto.style.textAlign = texto.Alineamiento;
+            nuevoTexto.style.fontFamily = `'${texto.Fuente.Nombre}-${texto.Fuente.Modo}'`;
+
+            if (texto.Sombra.Display)
+                nuevoTexto.style.textShadow = `${texto.Sombra.Color} ${texto.Sombra.DesplazamientoX} ${texto.Sombra.DesplazamientoY} ${texto.Sombra.RadioDesenfoque}`;
+
+            if (texto.Contorno.Display) {
+                nuevoTexto.style.padding = texto.Contorno.Padding;
+                nuevoTexto.style.background = texto.Contorno.Color;
+                nuevoTexto.style.borderRadius = texto.Contorno.Redondeo;
+                if (texto.Contorno.Sombra.Display)
+                    nuevoTexto.style.boxShadow = `${texto.Contorno.Sombra.DesplazamientoX} ${texto.Contorno.Sombra.DesplazamientoY} ${texto.Contorno.Sombra.RadioDesenfoque} ${texto.Contorno.Sombra.Aumento} ${texto.Contorno.Sombra.Color}`;
+            }
+            element.appendChild(nuevoTexto);
+
         }
 
-        nuevoTexto.style.fontSize = texto.Tamano;
-        nuevoTexto.style.color = texto.Color;
-        nuevoTexto.style.opacity = texto.Opacidad;
-        nuevoTexto.style.textAlign = texto.Alineamiento;
-        nuevoTexto.style.fontFamily = `'${texto.Fuente.Nombre}-${texto.Fuente.Modo}'`;
-
-        if (texto.Sombra.Display)
-            nuevoTexto.style.textShadow = `${texto.Sombra.Color} ${texto.Sombra.DesplazamientoX} ${texto.Sombra.DesplazamientoY} ${texto.Sombra.RadioDesenfoque}`;
-
-        if (texto.Contorno.Display) {
-            nuevoTexto.style.padding = texto.Contorno.Padding;
-            nuevoTexto.style.background = texto.Contorno.Color;
-            nuevoTexto.style.borderRadius = texto.Contorno.Redondeo;
-            if (texto.Contorno.Sombra.Display)
-                nuevoTexto.style.boxShadow = `${texto.Contorno.Sombra.DesplazamientoX} ${texto.Contorno.Sombra.DesplazamientoY} ${texto.Contorno.Sombra.RadioDesenfoque} ${texto.Contorno.Sombra.Aumento} ${texto.Contorno.Sombra.Color}`;
-        }
-        element.appendChild(nuevoTexto);
+        // Actualiza el índice
+        IdxImagen++;
+        if (IdxImagen == OrdenFotos.length)
+            IdxImagen = 0;
+        ActualizarTextos();
 
     }
-
-    // Actualiza el índice
-    IdxImagen++;
-    if (IdxImagen == OrdenFotos.length)
-        IdxImagen = 0;
-    ActualizarTextos();
 
 }
 
@@ -103,7 +117,7 @@ var CambiarImagen = async () => {
 
     setTimeout(CambiarImagen, Config.Delay * 1000);
     console.log("Imagen cambiada.");
-    document.querySelector("body").style.setProperty("--Showed", (IdxImagen+1) / OrdenFotos.length * 100);
+    document.querySelector("body").style.setProperty("--Showed", (IdxImagen + 1) / OrdenFotos.length * 100);
     await CargarImagen("div.Reloj.Waiting");
 
 }
